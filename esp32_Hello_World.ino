1af00b7406ebf5a12242603bedfc6f6bfd00736c
Andrew Adams <53242232+AndyCAdams@users.noreply.github.com>
2026-09-01 10:25:33 -0700

    HelloWorld
    getting ESP32 powered on, connected to computer, and a quick verification/sanity check to verify that the temu-knockoff has sane behavior

---
+++ esp32_Hello_World.ino
@@ -0,0 +1,12 @@
+#define LED_PIN 15  // onboard blue status LED
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+}
