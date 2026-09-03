72e8f18b87f204acb3a441070f2267f1aee516c2
AndyADskTp <AndyCAdams2011@gmail.com>
2026-09-02 22:27:05 -0700

    Wifi bringup and secret file integration

--- BonsaiStatistics.ino
+++ BonsaiStatistics.ino
@@ -1,12 +1,37 @@
-#define LED_PIN 15  // onboard blue status LED
+#include <WiFi.h>
+#include "secrets.h"
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Starting WiFi connection test...");
+  Serial.print("Connecting to: ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  
+  unsigned long startAttempt = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("WiFi status code: ");
+    Serial.println(WiFi.status());
+
+    // fail out after 15 seconds instead of hanging forever
+    if (millis() - startAttempt > 15000) {
+      Serial.println("\nFailed to connect. Check credentials or signal.");
+      return;
+    }
+  }
+
+  Serial.println("\nConnected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Signal strength (RSSI): ");
+  Serial.print(WiFi.RSSI());
+  Serial.println(" dBm");
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
+  //NULL
 }
