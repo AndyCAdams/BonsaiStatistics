8bec540c7d0da51134bb83c4fd75d399c01e3b6f
AndyADskTp <AndyCAdams2011@gmail.com>
2026-09-04 16:03:00 -0700

    established successful connection with influxDB

--- BonsaiStatistics.ino
+++ BonsaiStatistics.ino
@@ -1,37 +1,58 @@
 #include <WiFi.h>
-#include "secrets.h"
+#include <HTTPClient.h>
+#include "secrets.h"  // needs: ssid, password, influx_url, influx_token, influx_org, influx_bucket
+
+#define LED_PIN 15  // onboard blue status LED
+#define SENSOR_PIN 1 //placeholder
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  Serial.println("Starting WiFi connection test...");
-  Serial.print("Connecting to: ");
-  Serial.println(ssid);
-
   WiFi.begin(ssid, password);
-  
-  unsigned long startAttempt = millis();
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print("WiFi status code: ");
-    Serial.println(WiFi.status());
-
-    // fail out after 15 seconds instead of hanging forever
-    if (millis() - startAttempt > 15000) {
-      Serial.println("\nFailed to connect. Check credentials or signal.");
-      return;
-    }
+    Serial.print(".");
   }
-
-  Serial.println("\nConnected!");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("Signal strength (RSSI): ");
-  Serial.print(WiFi.RSSI());
-  Serial.println(" dBm");
+  Serial.println("\nConnected! IP: " + WiFi.localIP().toString());
 }
 
 void loop() {
-  //NULL
+  int rawValue = analogRead(SENSOR_PIN);
+  int httpCode;
+  
+  if (WiFi.status() == WL_CONNECTED) {
+
+    HTTPClient http;
+
+    // Build the full write URL: endpoint + org + bucket + precision
+    String url = String(influx_url) + "/api/v2/write?org=" + influx_org +
+                 "&bucket=" + influx_bucket + "&precision=s";
+
+    http.begin(url);
+    http.addHeader("Authorization", "Token " + String(influx_token));
+    http.addHeader("Content-Type", "text/plain; charset=utf-8");
+
+    // Line Protocol format: measurement,tag=value field=value
+    String lineProtocol = "soil_moisture,location=patio value=" + String(rawValue);
+
+    httpCode = http.POST(lineProtocol);
+
+    Serial.print("Sent: ");
+    Serial.println(lineProtocol);
+    Serial.print("Response code: ");
+    Serial.println(httpCode);  // 204 = success for InfluxDB writes
+
+    if (httpCode != 204) {
+      Serial.println(http.getString());  // print error body if something's wrong
+    }
+
+    http.end();
+  } else {
+    Serial.println("NoCon");
+    blink();
+  }
+
+  delay(1000);
 }
